26981f4225102cf838631d9fce487f8741860d9a
Abdulaziz Aljadaan <111672775+aljadan@users.noreply.github.com>
2024-07-26 15:20:02 +0300

    feat: add event listener

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
-// Import the native module. On web, it will be resolved to VpnDetector.web.ts
-// and on native platforms to VpnDetector.ts
+import NetInfo from "@react-native-community/netinfo";
+import { useEffect, useState } from "react";
+
 import VpnDetectorModule from "./VpnDetectorModule";
 
 export function isVpnActive(): boolean {
   return VpnDetectorModule.isVpnActive();
 }
+
+export function addEventListener(listener: (isVpnActive: boolean) => void) {
+  let previousIsVpnActive: boolean | undefined;
+  return NetInfo.addEventListener(() => {
+    const currentIsVpnActive = isVpnActive();
+    if (previousIsVpnActive !== currentIsVpnActive) {
+      listener(currentIsVpnActive);
+      previousIsVpnActive = currentIsVpnActive;
+    }
+  });
+}
+
+export const useIsVpnActive = () => {
+  const [isVpnActiveValue, setIsVpnActive] = useState(() => isVpnActive());
+
+  useEffect(() => addEventListener(setIsVpnActive), []);
+
+  return isVpnActiveValue;
+};
