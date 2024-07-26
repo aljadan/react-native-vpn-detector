2f79537765ac6aa77fbf0348917af8c2b8b99761
Abdulaziz Aljadaan <111672775+aljadan@users.noreply.github.com>
2024-07-26 16:46:03 +0300

    feat: add TSDoc

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,19 @@ import { useEffect, useState } from "react";
 
 import VpnDetectorModule from "./VpnDetectorModule";
 
+/**
+ * Checks if a VPN connection is active.
+ * @returns {boolean} Returns true if a VPN connection is active, otherwise false.
+ */
 export function isVpnActive(): boolean {
   return VpnDetectorModule.isVpnActive();
 }
 
+/**
+ * Adds an event listener for VPN connection status changes.
+ * @param listener - A callback function that is invoked when the VPN status changes.
+ * @returns A function to unsubscribe the event listener.
+ */
 export function addEventListener(listener: (isVpnActive: boolean) => void) {
   let previousIsVpnActive: boolean | undefined;
   return NetInfo.addEventListener(() => {
@@ -18,8 +27,14 @@ export function addEventListener(listener: (isVpnActive: boolean) => void) {
   });
 }
 
-export const useIsVpnActive = () => {
-  const [isVpnActiveValue, setIsVpnActive] = useState(() => isVpnActive());
+/**
+ * Custom React hook to get the current VPN connection status.
+ * @returns Returns true if a VPN connection is active, otherwise false.
+ */
+export const useIsVpnActive = (): boolean => {
+  const [isVpnActiveValue, setIsVpnActive] = useState<boolean>(() =>
+    isVpnActive(),
+  );
 
   useEffect(() => addEventListener(setIsVpnActive), []);
 
